9648a37a370959e920a0188b953e00d040059af1
Krae <kraewind@gmail.com>
2021-03-18 16:35:28 -0400

    Made a helper method for the parties index

--- app/helpers/parties_helper.rb
+++ app/helpers/parties_helper.rb
@@ -1,2 +1,11 @@
 module PartiesHelper
+    def parties_or_not(obj)
+        if obj.empty?
+            tag.h2 class: "empty list" do
+                "There are no resources. GO CREATE ONE!"
+            end
+        else
+            render obj
+        end
+    end
 end
